b2d2caf4ef34afbb9e6214e30be3e198110faabe
braimor <briangmsosa@gmail.com>
2022-08-25 21:25:38 -0300

    First commit

--- src/ConsoleApplication/Program.cs
+++ src/ConsoleApplication/Program.cs
@@ -19,9 +19,27 @@ namespace ConsoleApplication
         /// </summary>
         public static void Main()
         {
-            var train = new Train();
-            train.StartEngines();
-            Console.WriteLine("Hello World!");
+            /// 100 instancias de la clase Tren
+            for (int i = 0; i < 100; i++) 
+            {
+                var train = new Train($"train{i}");
+            }
+
+            /// 10.000.000 de instancias
+            for (int i = 0; i < 10000000; i++) 
+            {
+                var train = new Train($"train{i}");
+                train.StartEngines();
+            }
+
+            ///  3 instancias de la clase Train, t1, t2, t3
+            var t1 = new Train("Last Train To London");
+            var t2 = new Train("Last Train To London");
+            var t3 = new Train("Runaway Train");
+
+            ///  t1==t2 y t2 == t3
+            /// t1 no es igual a t2 ni t2 es igual a t3 porque apuntan a distintos espacios en memoria
+            /// 
         }
     }
 }
